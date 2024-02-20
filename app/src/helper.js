9d2c8724421035f63d505597fba2a926f8fced9b
mayayr115 <mayaramkishuncsh2027@gmail.com>
2024-02-20 09:20:48 -0500

    Submit Works

--- app/src/helper.js
+++ app/src/helper.js
@@ -1,1 +1,6 @@
 import './style.css'
+import initialTodos from './todos.json';
+
+initialTodos.forEach((todo) => {
+  document.querySelector('#app').innerHTML += `<li>${todo.title}</li>`;
+});
--- app/src/main.js
+++ app/src/main.js
@@ -1,2 +1,24 @@
 import './style.css'
 import './helper.js'
+import { v4 as uuidv4 } from 'uuid';
+
+const handleNewTodo = (e) => {
+  e.preventDefault();
+
+  const form = e.target;
+  const newTodo = {
+    uuid: uuidv4(),
+    title: form.todoTitle.value,
+    isComplete: false
+  }
+  
+  console.log(newTodo);
+
+  form.reset();
+};
+
+const main = () => {
+  document.querySelector("form#new-todo-form").addEventListener('submit', handleNewTodo);
+};
+
+main();
