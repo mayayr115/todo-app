97f5b6b85043d9fe053ea4c4e6fcabe04e1f3d2a
mayayr115 <mayaramkishuncsh2027@gmail.com>
2024-02-20 09:09:04 -0500

    Starting All JS Over!

--- app/src/main.js
+++ app/src/main.js
@@ -1,25 +1,2 @@
 import './style.css'
-import initialTodos from'./todos.json';
-import { v4 as uuidv4 } from 'uuid';
 import './helper.js'
-
-const handleNewTodo = (e) => {
-  e.preventDefault();
-  
-  const form = e.target;
-  const newTodo = {
-    uuid: uuidv4(),
-    title: form.todoTitle.value,
-    isComplete: false
-  }
-  
-  console.log(newTodo);
-  
-  form.reset();
-};
-
-initialTodos.forEach((todo) => {
-  document.querySelector('#app').innerHTML += `<li>${todo.title}</li>`;
-});
-
-document.querySelector("form#new-todo-form").addEventListener('submit', handleNewTodo);
